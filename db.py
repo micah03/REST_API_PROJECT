9bc119c40c00adadf7f4d5bbb7a9106d479e8c75
Mayukha Mohan Thatikonda <mayukhamohan@192.168.1.3>
2024-12-06 19:10:27 +0530

    Initial commit: Set up REST API project structure

---
+++ db.py
@@ -0,0 +1,23 @@
+# stores = [
+    # {
+    #     "name": "My Store",
+    #     "items": [
+    #         {
+    #             "name": "Chair",
+    #             "price": 15.99
+    #         }
+    #     ]
+    # }
+# ]
+
+stores = {}
+items = {
+    1: {
+        "name": "Chair",
+        "price": 17.99
+    },
+    2: {
+        "name": "Table",
+        "price": 180.50
+    }
+}
